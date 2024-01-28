1449cb4860308a5fa8423ef988eea8e73e11553c
hasan <yasashrafi@gmail.com>
2024-01-28 15:33:47 +0330

    create buttons and import increment & decrement

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -1,14 +1,16 @@
-import { useSelector } from 'react-redux';
-import { selectCounter } from '../features/counter/counterSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment, selectCounter } from '../features/counter/counterSlice';
 
 function Counter() {
     const counter = useSelector(selectCounter)
-
+    const dispatch = useDispatch(increment, decrement)
 
     return (
         <div>
-            Counter
+           
             <h2>counter is: {counter}</h2>
+            <button onClick={() => dispatch(increment())}>Increase +</button>
+            <button onClick={() => dispatch(decrement())}>Decrease -</button>
         </div>
     )
 }
