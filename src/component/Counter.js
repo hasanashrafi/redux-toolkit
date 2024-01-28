f1008bf4e4fce9a666b850f0f5c24b6729551fa0
hasan <yasashrafi@gmail.com>
2024-01-28 15:44:24 +0330

    increment by amount and use usestate

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -1,16 +1,20 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { decrement, increment, selectCounter } from '../features/counter/counterSlice';
+import { decrement, increment, incrementByAmount, selectCounter } from '../features/counter/counterSlice';
+import { useState } from 'react';
 
 function Counter() {
+    const [value, setValue] = useState(2)
+
     const counter = useSelector(selectCounter)
-    const dispatch = useDispatch(increment, decrement)
+    const dispatch = useDispatch(increment, decrement, incrementByAmount)
 
     return (
         <div>
-           
-            <h2>counter is: {counter}</h2>
+            <h2>Counter is : {counter}</h2>
             <button onClick={() => dispatch(increment())}>Increase +</button>
             <button onClick={() => dispatch(decrement())}>Decrease -</button>
+            <button onClick={() => dispatch(incrementByAmount(+value))}>increment By Amount +5</button>
+            <input type='number' value={value} onChange={e => setValue(e.target.value)} />
         </div>
     )
 }
