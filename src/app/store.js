a4a6dd8438bc170035c90a4cf1870f9dc0ec1e37
hasan <yasashrafi@gmail.com>
2024-01-29 12:35:44 +0330

    added users reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 
-
 import counterReducer from "../features/counter/counterSlice"
 import numberReducer from "../features/number/numberSlice"
+import usersReducer from "../features/users/usersSlice"
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
         number: numberReducer,
+        users: usersReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(logger),
